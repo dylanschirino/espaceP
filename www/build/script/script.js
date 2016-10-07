52ffb6549a94f64599d155a076193944a3a4ed8c
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-10-07 22:43:36 +0200

    Adding rapport page with responsive

--- www/build/script/script.js
+++ www/build/script/script.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 
     (function($) {
 
+      var $button  = $("#button"),
+          $window    = $(window),
+          offset     = $button.offset(),
+          topPadding = 15;
+
         $('#header__burger').click(function(e){
             e.preventDefault();
             $('body').toggleClass('with--sidebar');
@@ -9,6 +14,19 @@ $(document).ready(function(){
 
     $('#site-cache').click(function(e){
       $('body').removeClass('with--sidebar');
+    });
+
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.bottom) {
+            $button.stop().animate({
+                marginTop: $window.scrollTop() - offset.bottom + topPadding
+            });
+        } else {
+            $button.stop().animate({
+                marginTop: 0
+            });
+        }
     });
 
     })(jQuery);
