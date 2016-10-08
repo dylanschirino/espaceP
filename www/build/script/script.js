37c2f2ce71121b6560a35ce03f8b0f90771c8c26
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-10-08 15:01:11 +0200

    Adding client page with responsive

--- www/build/script/script.js
+++ www/build/script/script.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
 
     (function($) {
 
-      var $button  = $("#button"),
-          $window    = $(window),
-          offset     = $button.offset(),
-          topPadding = 15;
 
         $('#header__burger').click(function(e){
             e.preventDefault();
@@ -14,19 +10,6 @@ $(document).ready(function(){
 
     $('#site-cache').click(function(e){
       $('body').removeClass('with--sidebar');
-    });
-
-
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.bottom) {
-            $button.stop().animate({
-                marginTop: $window.scrollTop() - offset.bottom + topPadding
-            });
-        } else {
-            $button.stop().animate({
-                marginTop: 0
-            });
-        }
     });
 
     })(jQuery);
