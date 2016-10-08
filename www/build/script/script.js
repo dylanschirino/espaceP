dbe20280fbeaa1936ac1b3d23acd015be5b107b2
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-10-08 22:28:43 +0200

    Add fixed sidebar( no-responsive

--- www/build/script/script.js
+++ www/build/script/script.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
 
     (function($) {
 
+      //Aside fixe
+
+        if ( $(window).width() > 1231) {
+          $('#sticky').scrollToFixed({ limit: $($('.actuality__article h3')[1]).offset().top });
+      }
+      else{
+        $(window).resize(function(){
+          $('aside').addClass('no-sticky');
+          $('.no-sticky').trigger('resize');
+        });
+      }
+
+
+        // Menu burger
 
         $('#header__burger').click(function(e){
             e.preventDefault();
