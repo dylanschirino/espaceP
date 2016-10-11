0ea3f384ebf8423b4e647522f59e5e7ee8d78fc6
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-10-11 17:25:41 +0200

    Adding actu heading with coherence as asking

--- www/build/script/script.js
+++ www/build/script/script.js
@@ -1,30 +1,38 @@
 
+$(document).ready(function(){
+  // Menu burger
+  (function($){
 
-    (function($) {
+  })(jQuery);
+  $('#header__burger').click(function(e){
+    e.preventDefault();
+    $('body').toggleClass('with--sidebar');
+  });
+
+  $('#site-cache').click(function(e){
+    $('body').removeClass('with--sidebar');
+  });
+});
+
+
+(function($) {
+
+
+
+  $('#sticky').scrollToFixed({ limit: $($('.actuality__article h3')[1]).offset().top });
+  //Aside fixe
+  $(window).on('resize', function(){
+    if ( $(window).width() < 1251) {
+      //$.ScrollToFixed=null;
+      $('aside').trigger('detach.ScrollToFixed');
+      $('body').addClass('no-sticky')
+    }
+    else{
+      $('aside').attr('id',"sticky");
+      $('body').removeClass('no-sticky');
       $('#sticky').scrollToFixed({ limit: $($('.actuality__article h3')[1]).offset().top });
-      //Aside fixe
-      $(window).on('resize', function(){
-        if ( $(window).width() < 1251) {
-          //$.ScrollToFixed=null;
-          $('aside').trigger('detach.ScrollToFixed');
-          $('body').addClass('no-sticky')
-      }
-      else{
-        $('aside').attr('id',"sticky");
-        $('body').removeClass('no-sticky');
-        $('#sticky').scrollToFixed({ limit: $($('.actuality__article h3')[1]).offset().top });
-      }
-    });
+    }
+  });
 
-        // Menu burger
 
-        $('#header__burger').click(function(e){
-            e.preventDefault();
-            $('body').toggleClass('with--sidebar');
-        });
-
-    $('#site-cache').click(function(e){
-      $('body').removeClass('with--sidebar');
-    });
-
-    })(jQuery);
+})(jQuery);
